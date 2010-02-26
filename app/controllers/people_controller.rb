e3da5089104f82681f71e7b606bdeaa9f055b98a
Lachlan Sylvester <lachlan.sylvester@hypothetical.com.au>
2010-02-26 22:43:56 +1100

    grr

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,6 @@
 class PeopleController < ApplicationController
   def show
     @person = Person.find(params[:id])
-    @upcoming_reminders = @person.reminders.upcoming
-    @past_reminders = @person.reminders.past
     @reminder = @person.reminders.build
   end
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -2,10 +2,10 @@ class Reminder < ActiveRecord::Base
   belongs_to :person
   
   named_scope :upcoming, lambda {
-    {:conditions => ["send_at > ?", Time.zone.now]}
+    {:conditions => ["send_at > ?", Time.zone.now], :order => "send_at asc"}
   }
   
   named_scope :past, lambda {
-    {:conditions => ["send_at <= ?", Time.zone.now]}
+    {:conditions => ["send_at <= ?", Time.zone.now], :order => "send_at desc"}
   }
 end
